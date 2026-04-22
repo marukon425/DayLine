3174c4bf437344968d3ab01483857359f5433fc2
marukon425 <maruriku0655@gmail.com>
2026-04-22 20:41:39 +0900

    build.sh修正

--- build.sh
+++ build.sh
@@ -4,9 +4,3 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py shell -c "
-from django.contrib.auth import get_user_model
-User = get_user_model()
-if not User.objects.filter(email='admin@example.com').exists():
-    User.objects.create_superuser(username='admin', email='maruriku0655@gmail.com', password='admin1')
-"
