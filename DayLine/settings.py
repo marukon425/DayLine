36acc5c6400fac877febe95140f27d448288d0d1
marukon425 <maruriku0655@gmail.com>
2026-04-23 10:36:02 +0900

    デバッグオン

--- DayLine/settings.py
+++ DayLine/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fmkooc)(4@u#1)zl!93=ho&je34o2!$gpor!o1do3(qvgjj=d='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
