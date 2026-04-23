337a02bfe47c88915b125507014b2f7e6cc13b71
marukon425 <maruriku0655@gmail.com>
2026-04-24 01:07:12 +0900

    スーパーユーザー作成

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(email='maruriku0655@gmail.com').exists():
+    User.objects.create_superuser(email='maruriku0655@gmail.com', password='admin', username='admin')
+"
