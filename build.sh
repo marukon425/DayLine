b594cb201d4d9b97a84d6ca652e8bfaa1f89453a
marukon425 <maruriku0655@gmail.com>
2026-04-24 01:16:52 +0900

    修正

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ python manage.py shell -c "
 from django.contrib.auth import get_user_model
 User = get_user_model()
 if not User.objects.filter(email='maruriku0655@gmail.com').exists():
-    User.objects.create_superuser(email='maruriku0655@gmail.com', password='admin', username='admin')
+    User.objects.create_superuser(email='maruriku0655@gmail.com', username='admin', password='admin')
+print('done')
 "
