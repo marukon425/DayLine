a2f4ae4901c34d6fbe27b4f1e1a512b60387781e
marukon425 <maruriku0655@gmail.com>
2026-05-11 00:38:25 +0900

    s3

--- DayLine/settings/base.py
+++ DayLine/settings/base.py
@@ -236,7 +236,14 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
 
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 
