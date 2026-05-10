fa79c98f9dbfb162fbcb6e783ae913f6e6565dbb
marukon425 <maruriku0655@gmail.com>
2026-05-10 22:07:50 +0900

    s3設定変更

--- DayLine/settings/base.py
+++ DayLine/settings/base.py
@@ -239,3 +239,8 @@ AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+
+AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
