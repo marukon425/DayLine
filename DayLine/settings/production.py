9b02e48730d1bf9ed2774f9a4110c31d674cd547
marukon425 <maruriku0655@gmail.com>
2026-05-10 22:47:44 +0900

    プロダクト設定変更

--- DayLine/settings/production.py
+++ DayLine/settings/production.py
@@ -14,6 +14,6 @@ DATABASES = {
     }
 }
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 CSRF_TRUSTED_ORIGINS = [os.environ.get('CSRF_TRUSTED_ORIGINS')]
