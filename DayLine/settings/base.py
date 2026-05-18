b42a11e984f735e973f70d20f07cb62d1366fb42
marukon425 <maruriku0655@gmail.com>
2026-05-18 11:22:40 +0900

    settings/base修正

--- DayLine/settings/base.py
+++ DayLine/settings/base.py
@@ -76,7 +76,7 @@ ROOT_URLCONF = 'DayLine.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'DayLine' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
